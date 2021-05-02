7207512a6063a1d53fc23c44b2a353cac4b4bd32
dorsic <marek.dorsic@centrum.sk>
2021-05-02 11:40:25 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # PicoDIV
 Raspberry Pi Pico frequency divider
+
+Divides the input frequency by factor 10^7, 10MHz to 1Hz using PIO.
+System clock has to be driven from 10MHz external clock through GPIO.
+Based on the [pio_blink example] (https://github.com/raspberrypi/pico-examples/tree/master/pio/pio_blink)
+
+Connect 10 MHz 3.3V input signal to GPIO20 (pin 26). Output 1 Hz, 50-50 duty cycle signal is generated on GPIO0 (pin 1) and GPIO25 - LED pin, which then starts to blink.
+
+### Pico programming
+Connect the Pico while holding down the button on the Pico board to USB.
+Drag and drop the *.uf2 file to Pico board.
+
+In case of problems consult the great Raspberry Pi Pico [Getting Started Guide] (https://www.raspberrypi.org/documentation/rp2040/getting-started/#getting-started-with-c).
+
+### Compilation
+#### MacOS
+Get you environment ready by [installing the toolchain] (https://datasheets.raspberrypi.org/pico/getting-started-with-pico.pdf)
+
+Setup local environment variables
+```
+export PATH="$PATH:/Applications/microchip/xc8/v2.31/bin"
+export PICO_TOOLCHAIN_PATH=/Applications/ARM/bin
+export PICO_SDK_PATH=<pico-sdk>
+```
+
+Change to build directory
+```
+cd ./build
+```
+
+Run commands to compile and build
+```
+cmake ../
+make
+```
+
+After that you should find `picoDIV.uf2` file in the build directory that can be used to program your board.
---
+++ picoDIV.c
@@ -0,0 +1,48 @@
+/*
+    Divides the input frequency by factor 10^7, 10MHz to 1Hz using PIO.
+    System clock has to be driven from 10MHz external clock through GPIO.
+    Based on the pio_blink example https://github.com/raspberrypi/pico-examples/tree/master/pio/pio_blink
+
+    Connect 10 MHz 3.3V input signal to GPIO20 (pin 26).
+    Output 1 Hz, 50-50 duty cycle signal is generated on GPIO0 (pin 1) 
+    and GPIO25 - LED pin, which then starts to blink.
+
+    SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "hardware/pll.h"
+#include "picoDIV.pio.h"
+
+
+uint input_freq = 10000000; // 10 MHz
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    picodiv_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+    
+//    pio->txf[sm] = 5000000-3;     // for 10MHz to 1Hz division
+    pio->txf[sm] = (input_freq/(2*freq))-3;
+}
+
+int main() {
+
+    // configure the clk_ref to run from pin GPIO20 (pin 26)
+    clock_configure_gpin(clk_ref, 20, input_freq, input_freq);
+
+    // configure the clk_sys, which drives the PIO to run from pin GPIO20 (pin 26)
+    clock_configure_gpin(clk_sys, 20, input_freq, input_freq);
+
+    // stop the clk_sys PPL
+    pll_deinit(pll_sys);
+
+    // program the PIOs
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &picodiv_program);
+
+    // assign output pins and outout frequency to PIOs
+    blink_pin_forever(pio, 0, offset, 0, 1);        // output on GPIO0 (pin 1)
+    blink_pin_forever(pio, 1, offset, 25, 1);       // blink also the LED on GPIO25
+}
