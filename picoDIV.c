3245acfaf10410b425f7390e2a21d93c824873ef
dorsic <marek.dorsic@centrum.sk>
2021-05-08 00:02:19 +0200

    added variable pulse length

--- picoDIV.c
+++ picoDIV.c
@@ -10,7 +10,7 @@
     SPDX-License-Identifier: BSD-3-Clause
 
     Version:
-    02-May-2021  Marek Dorsic (.md)
+    07-May-2021  Marek Dorsic (.md)
 */
 
 #include "pico/stdlib.h"
@@ -18,25 +18,36 @@
 #include "hardware/pio.h"
 #include "hardware/pll.h"
 #include "picoDIV.pio.h"
-
+#include "hardware/xosc.h"
 
 uint input_freq = 10000000; // 10 MHz
+uint pulse_len = 100000;    // number of clock cycles of the output pulse length
+                            // 100000 cycles * 100ns (for 10MHz) = 10ms
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint total_clk, uint pulse_clk) {
     picodiv_program_init(pio, sm, offset, pin);
     pio_sm_set_enabled(pio, sm, true);
     
-//    pio->txf[sm] = 5000000-3;     // for 10MHz to 1Hz division
-    pio->txf[sm] = (input_freq/(2*freq))-3;
+    pio->txf[sm] = pulse_clk - 3;                   // write number of HIGH clock cycles to PIO register
+    pio->txf[sm] =  total_clk - pulse_clk - 3;      // write number off LOW clock cycles to PIO register
 }
 
 int main() {
 
+/*  COMMENT THIS OUT IF YOU WANT TO USE THE INTERNAL XOSC AS REFERENCE CLOCK */
     // configure the clk_ref to run from pin GPIO20 (pin 26)
     clock_configure_gpin(clk_ref, 20, input_freq, input_freq);
 
     // configure the clk_sys, which drives the PIO to run from pin GPIO20 (pin 26)
     clock_configure_gpin(clk_sys, 20, input_freq, input_freq);
+
+/*  UNCOMMENT THIS IF YOU WANT TO USE THE INTERNAL XOSC AS REFERENCE CLOCK 
+    xosc_init();
+    input_freq = 12000000;  // 12MHz is the internal XOSC
+    pulse_len = 120000;     // 10ms pulse
+    clock_configure(clk_ref, CLOCKS_CLK_REF_CTRL_SRC_VALUE_XOSC_CLKSRC, 0, input_freq, input_freq);
+    clock_configure(clk_sys, CLOCKS_CLK_SYS_CTRL_SRC_VALUE_CLK_REF, CLOCKS_CLK_SYS_CTRL_AUXSRC_VALUE_XOSC_CLKSRC, input_freq, input_freq);
+*/
 
     // stop the clk_sys PPL
     pll_deinit(pll_sys);
@@ -46,6 +57,8 @@ int main() {
     uint offset = pio_add_program(pio, &picodiv_program);
 
     // assign output pins and outout frequency to PIOs
-    blink_pin_forever(pio, 0, offset, 0, 1);        // output on GPIO0 (pin 1)
-    blink_pin_forever(pio, 1, offset, 25, 1);       // blink also the LED on GPIO25
+    blink_pin_forever(pio, 0, offset, 0, input_freq, pulse_len);    // 1 PPS output on GPIO0 (pin 1)
+    blink_pin_forever(pio, 1, offset, 25, input_freq, pulse_len);   // blink the LED on GPIO25
+    blink_pin_forever(pio, 2, offset, 21, 10, 5);                   // 1 MHz output on GPIO21 (pin 27) 50/50 square
+    blink_pin_forever(pio, 3, offset, 15, 100, 50);                 // 100 kHz output on GPIO15 (pin 20) 50/50 square
 }
